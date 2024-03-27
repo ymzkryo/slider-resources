a1ec1d06a5a93439d002cfc9687f7a169ac7588c
ymzkryo <ymzk.yamachan@gmail.com>
2024-03-27 22:50:35 +0900

    Update

--- scripts/slidev-export-pdf-dark.js
+++ scripts/slidev-export-pdf-dark.js
@@ -1,18 +1,20 @@
-// scripts/export-pdf.js
 const { execSync } = require('child_process');
 const fs = require('fs');
-const path = process.argv[2];
 
-if (!path) {
-  console.log('Usage: yarn export-pdf <path-to-directory>');
-  process.exit(1);
-}
+// 引数が提供されなかった場合、条件に合うディレクトリを検索
+const targetDirectories = process.argv[2]
+  ? [process.argv[2]]
+  : fs.readdirSync('.')
+      .filter(f => fs.statSync(f).isDirectory() && f.match(/^\d{4}-\d{2}-\d{2}/));
 
-try {
-  // Slidevのエクスポートコマンドを実行してPDFを生成
-  execSync(`yarn slidev export --dark  ${path}/slides.md --output dist/${path.split('/').pop()}-dark.pdf`, { stdio: 'inherit' });
-  console.log(`Exported PDF to ${path}/${path.split('/').pop()}.pdf`);
-} catch (error) {
-  console.error('Failed to export Slidev to PDF:', error);
-}
-
+targetDirectories.forEach((path) => {
+  try {
+    // ディレクトリ名からPDFファイル名を生成
+    const pdfName = `${path.split('/').pop()}-dark.pdf`;
+    // Slidevのエクスポートコマンドを実行してPDFを生成
+    execSync(`yarn slidev export --dark ${path}/slides.md --output dist/${pdfName}`, { stdio: 'inherit' });
+    console.log(`Exported PDF to dist/${pdfName}`);
+  } catch (error) {
+    console.error(`Failed to export Slidev to PDF for ${path}:`, error);
+  }
+});
